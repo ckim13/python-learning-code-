c2fb3c2046c40fad3e68c9144a490f10ac5d6dbb
ckim13 <61024218+ckim13@users.noreply.github.com>
2020-02-18 14:23:38 -0500

    Add files via upload

---
+++ simple variable test 1.py
@@ -0,0 +1,8 @@
+
+>>> first_name = "Madison"
+>>> last_name = "Sarepth"
+>>> full_name = first_name + " " + last_name
+>>> message = "Hello, " + full_name.title() + "!"
+>>> print(message)
+Hello, Madison Sarepth!
+>>> 
