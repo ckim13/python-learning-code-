42e8d66e6a23554f6a2a6a0ed9946a582a4c3236
ckim13 <61024218+ckim13@users.noreply.github.com>
2020-02-20 19:09:25 -0500

    message test v2

---
+++ personal message example.py
@@ -0,0 +1,6 @@
+first_name = "Madrice"
+last_name = "Guy"
+full_name = first_name + " " + last_name
+message = "Hello professor " + full_name.title() + "this a python test example have a pleasent day"
+print (message)
+
