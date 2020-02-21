d7b550fee103eab8375116078f093714a301d1f6
ckim13 <61024218+ckim13@users.noreply.github.com>
2020-02-21 10:32:23 -0500

    Add files via upload

---
+++ python comment example.py
@@ -0,0 +1,4 @@
+# say or # space say allows for comment to be written about the code in order ot rember just what was written. for example, str allows stringing of numbers into the code . the #say function ensures the first line of code is ignored and the following code is run 
+age = 27
+message = "Happy " + str(age) + "rd Birthday"
+print (message) 
