1f298fdc48bb7476b7f9a03f1920d0e33da1219f
ckim13 <61024218+ckim13@users.noreply.github.com>
2020-03-05 20:58:18 -0500

    Add files via upload

---
+++ white stripping commentary.py
@@ -0,0 +1,6 @@
+
+#notice that when the output is used there are whitespace between the qutotation marks
+
+# the .strip() removes that sopace and do this by either deploying rstrip for right and lstrip for left. .stip() command does it all.  
+
+#white stripping is used to clean up user input before it's stored in a program. 
