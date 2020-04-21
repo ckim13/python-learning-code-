3d81dfd390cb7ef7a920076b8653b97030f41853
ckim13 <61024218+ckim13@users.noreply.github.com>
2020-04-21 11:26:27 -0400

    Add files via upload

---
+++ looping items in a list example.py
@@ -0,0 +1,9 @@
+#listing items using the for and in functions allows python to loop through the items in a list 
+persons = [ 'alice', 'david', 'marry', 'mark', 'jenny', 'robert' ]
+for persons in persons:
+    print (persons .title ( ) +  ", that was a great trick! " ) 
+            
+#the ".\n" command inserts a new line in the loop
+    print ( "I can't wait to see your next trick, " + persons.title ( ) + ".\n")
+#any line that isn't indented is only executed once without repeating 
+print ( "thank you all so much for coming see you next week!") 
